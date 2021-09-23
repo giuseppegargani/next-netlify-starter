b1b4d683d364fd5bd5f704729e9dc4a6362376ed
Houde Giuseppe Gargani <51164844+giuseppegargani@users.noreply.github.com>
2021-09-23 05:16:29 +0200

    Eccomi

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Benvenuto alla mia App!" />
+        <Header title="Benvenuto alla mia App editabile!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
